2a9378105ca2c499d694f7d98558c1f841fbabc0
lph2o <abdou7hiam@gmail.com>
2024-05-04 17:57:37 +0000

    Init Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         <h1 className="text-3xl font-bold underline text-red-500">
-      Bonsoir
-    </h1>
-        </a>
-      </header>
+      
     </div>
   );
 }
